55a5108f301c7a8d5deac94fec99aedd78487cc2
EllaSun <sunyingms@gmail.com>
2016-07-10 11:52:21 +1200

    Update base_server.py

--- priceserver_autotest_pyunit/baseLib/base_server.py
+++ priceserver_autotest_pyunit/baseLib/base_server.py
@@ -1,5 +1,6 @@
-# coding:GBK
-import os,time
+import os,time,sys
+import string
+from baseLib import base_init
 
 class Server:
 	def __init__(self,start_script,app_name):
@@ -7,22 +8,22 @@ class Server:
 		self.app_name = app_name
 
 	def start(self):
-		'''启动Server'''
+		'''start server'''
 		if self.start_script == None:
 			return -1
 		os.system(self.start_script)
 		pid = os.popen("pidof " + self.app_name).readline().strip('\n')
 		n = 0
 		while self.getAppStatus().find("S")<0:
-			time.sleep(1)
-			if n > 30:
+			time.sleep(2)
+			if n>3:
 				return -1
-			n += 1
-		time.sleep(0.3)
+			n+=1
+		time.sleep(2)
 		return pid
 
 	def stop(self):
-		'''停止Server'''
+		'''stop server'''
 		os.system("killall -9 -q " + self.app_name)
 		pid = os.popen("pidof " + self.app_name).readline().strip('\n')
 		if pid == None or pid == "":
@@ -31,7 +32,6 @@ class Server:
 			return -1
 
 	def getAppStatus(self):
-		'''获取应用程序运行状态'''
+		'''get application runing status'''
 		cmd = "ps aux|grep " + self.app_name + "|grep -v grep |awk '{print $8}'"
 		return os.popen(cmd).readline()
-
